bd150de26b5c05ccab2911acc5cbb8cddbd6150f
Abdi Bacha <contactabdi.bacha@gmail.com>
2024-07-18 22:55:42 +0300

    update

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ function App() {
   const [theme, colorMode] = useMode();
   const [token, setToken] = useState(localStorage.getItem("token"));
 
+  
   return (
     <AuthContext.Provider value={{ token, setToken }}>
       <Router>
